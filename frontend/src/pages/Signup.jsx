f18680820ed9166ee62c4a2f64c51e779ed5c368
Rollins19-05-2003 <sdeb7433@gmail.com>
2025-02-28 15:23:54 +0530

    added url

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
     }
     try {
       // await axios.post(`http://localhost:${PORT}/signup`, { name, email, password });
-      await axios.post(`https://auth-project-topaz-eight.vercel.app/signup`, { name, email, password });
+      await axios.post(`https://auth-project-backend-roan.vercel.app/signup`, { name, email, password });
       toast.success("Signup successful! Redirecting to login...", { theme: "colored" });
       setTimeout(() => navigate("/login"), 3000); // Delay to allow flash message to be seen
     } catch (error) {
